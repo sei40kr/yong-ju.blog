d61ea6231d5c1dd52367005348e83c88a72e6f78
Seong Yong-ju <sei40kr@gmail.com>
2021-06-01 02:42:13 +0900

    Revert katex shortcode perf improvements

--- assets/auto-render.js
+++
@@ -1,9 +0,0 @@
-const mathElements = document.getElementsByClassName("js-katex");
-const displayMathElements = document.getElementsByClassName("js-katex-display");
-
-for (let element of mathElements) {
-  katex.render(element.textContent, element);
-}
-for (let element of displayMathElements) {
-  katex.render(element.textContent, element, { displayMode: true });
-}
